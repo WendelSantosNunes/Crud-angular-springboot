058c5d62c22d7d0958eeae8a12c2e54011b296f6
WendelSantosNunes <wendelnunes9999@gmail.com>
2023-12-27 17:58:08 -0300

    Feat: Adding icons to represent courses

--- src/app/shared/app-material/app-material.module.ts
+++ src/app/shared/app-material/app-material.module.ts
@@ -5,9 +5,10 @@ import { MatCardModule } from '@angular/material/card';
 import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatDialogModule} from '@angular/material/dialog';
 import { MatButtonModule} from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
-  exports: [MatToolbarModule, MatTableModule, MatCardModule, MatProgressSpinnerModule, MatDialogModule, MatButtonModule],
+  exports: [MatToolbarModule, MatTableModule, MatCardModule, MatProgressSpinnerModule, MatDialogModule, MatButtonModule, MatIconModule],
 })
 
 export class AppMaterialModule { }
